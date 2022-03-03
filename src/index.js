923f68db2655e52287b416f5e6d91f8ff925b600
DariusDark <darius99dark@mail.ru>
2022-03-03 14:50:01 +0500

    test: added ga

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga4';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+const TRAKING_ID = 'G-QGGLMJZ2EB';
+
+ReactGA.initialize(TRAKING_ID);
+ReactGA.send('pageview');
 
 ReactDOM.render(
   <Router>
